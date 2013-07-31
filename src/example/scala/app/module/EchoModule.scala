8079b8e8d1791881d088dd200cdcb8f0f95bbf88
Konstantin G Romanov <konstantin.romanov@inn.ru>
2013-07-31 18:49:41 +0400

    echo module cleaned

--- src/example/scala/app/module/EchoModule.scala
+++ src/example/scala/app/module/EchoModule.scala
@@ -2,12 +2,10 @@ package module
 
 import mlib.api._
 import mlib.impl.ChannelEventsActor.Event
-import akka.event.LoggingReceive
 import Message.message2JsValue
 
 class EchoModule extends Module {
-  def receive = LoggingReceive {
-    // Handling any message on cuckoo channel, and sending it back to client
+  def receive = {
     case Event(connection, message @ Message("echo", _, _)) => connection.send(message)
   }
 }
